c22dc49225bb67d6721df6f4cc177c0c247c0a70
root <root@ip-172-31-44-160.ec2.internal>
2023-03-30 16:18:56 -0300

    alteracoes no cleanLogs.sh

--- script/cleanLogs.sh
+++ script/cleanLogs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 log_dir="/caminho/para/diretorio/de/logs"
-max_logs=1000
+max_logs=5000
+deleted_logs=300
 
 num_logs=$(ls -1 "$log_dir" | wc -l)
 
 if [ $num_logs -gt $max_logs ]; then
-  logs_to_delete=$((num_logs - max_logs + 200))
+  logs_to_delete=$((num_logs - max_logs + deleted_logs))
   if [ $logs_to_delete -gt 0 ]; then
     cd "$log_dir" || exit
     ls -t | tail -$logs_to_delete | xargs rm --
