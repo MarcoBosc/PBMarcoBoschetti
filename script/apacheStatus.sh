126d126454a9f36bebaad5a25cc7743202c9e8cd
root <root@ip-172-31-44-160.ec2.internal>
2023-03-30 16:38:22 -0300

    alteracao da saida do script apacheStatus.sh

--- script/apacheStatus.sh
+++ script/apacheStatus.sh
@@ -12,7 +12,7 @@ filename=$(date +"%d-%m-%Y_%H:%M")_${status}_${message// /_}.txt
 
 filepath=/efs/marco/logs.apache/$filename
 
-echo "Data e Hora: $(date)" >> $filepath
+echo "Data e Hora: $(LC_TIME=pt_BR.UTF-8 date -d now '+%A, %d de %B de %Y - %H:%M:%S')" >> $filepath
 echo "Nome do Serviço: Apache" >> $filepath
 echo "Status: $status" >> $filepath
 echo "Mensagem: $message" >> $filepath
